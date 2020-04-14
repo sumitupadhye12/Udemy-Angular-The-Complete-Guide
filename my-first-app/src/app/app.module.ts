c5016b916ecaa885931acc73c9b966d170af60fc
sumitupadhye12 <sumitupadhye12@gmail.com>
2020-04-14 18:45:16 +0530

    bootstrap added

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,23 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.comonent';
-import { ServersComponent } from './servers/servers.component';
-import { SuccessSignalComponent } from './success-signal/success-signal.component';
-import { WarningAlertComponent } from "../warning-alert/warning-alert.component1";
+import { EventTestComponent } from './event-test/event-test.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessSignalComponent,
-    WarningAlertComponent,
+    EventTestComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
